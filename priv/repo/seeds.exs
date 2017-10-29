c525750ec4aea3ecd8d6167869a30e8256f1c541
Tijmen de Graaff <tijmen76@gmail.com>
2017-10-29 14:04:16 +0100

    create tag seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,7 +2,25 @@ alias Instagram.{Post, Repo}
 
 fake_photos = 10
 
+fake_tag = [
+    "Summer",
+    "Winter",
+    "Spring",
+    "Autumn",
+    "SundayNight",
+    "MondayMorning",
+]
+# Tags
+for name <- fake_tag do
+    %Post.Tag{name: name}
+    |> Repo.insert!
+end
+
+# Photos
 for _ <- 1..fake_photos do
-    %Post.Photo{image_url: Faker.Internet.image_url()}
+    %Post.Photo{
+        image_url: Faker.Internet.image_url(),
+        tags: Enum.take_random(Repo.all(Post.Tag), Enum.random(1..4))
+    }
     |> Repo.insert!
 end
