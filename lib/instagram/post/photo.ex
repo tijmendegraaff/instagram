76f54c12352e63869096faf89a396e74edf72103
Tijmen de Graaff <tijmen76@gmail.com>
2017-10-29 15:17:24 +0100

    add association between user and post

--- lib/instagram/post/photo.ex
+++ lib/instagram/post/photo.ex
@@ -7,13 +7,15 @@ defmodule Instagram.Post.Photo do
 
     many_to_many :tags, Instagram.Post.Tag, join_through: "photos_taggings"
 
+    belongs_to :user, Instagram.Account.User
+
     timestamps()
   end
 
   @doc false
   def changeset(%__MODULE__{} = photo, attrs) do
     photo
-    |> cast(attrs, [:image_url])
-    |> validate_required([:image_url])
+    |> cast(attrs, [:image_url, :user_id])
+    |> validate_required([:image_url, :user_id])
   end
 end
